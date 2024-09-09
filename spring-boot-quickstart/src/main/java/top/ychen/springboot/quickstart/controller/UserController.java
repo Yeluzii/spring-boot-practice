4013e616cb6bdc5f1af4d0072f2d8036a0eeb084
Yeluzii <2317413623@qq.com>
2024-09-09 13:19:36 +0800

    second commit

--- spring-boot-quickstart/src/main/java/top/ychen/springboot/quickstart/controller/UserController.java
+++ spring-boot-quickstart/src/main/java/top/ychen/springboot/quickstart/controller/UserController.java
@@ -3,6 +3,7 @@ package top.ychen.springboot.quickstart.controller;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import top.ychen.springboot.quickstart.service.UserService;
 
@@ -14,9 +15,13 @@ public class UserController {
     @Resource
     private UserService userService;
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/adults")
-    public List<String> getAdultUserName()
-    {
-        return userService.getAdultUserName();
+    public List<String> getAdultsUserName(
+            @RequestParam(value = "initial", required = false) String initial) {
+        return userService.getAdultsUserName(initial);
     }
 }
--- spring-boot-quickstart/src/main/java/top/ychen/springboot/quickstart/service/UserService.java
+++ spring-boot-quickstart/src/main/java/top/ychen/springboot/quickstart/service/UserService.java
@@ -10,16 +10,18 @@ import java.util.stream.Collectors;
 public class UserService {
     private final List<User> users = List.of(
             new User(1L, "张三",22),
-            new User(1L, "张三2",22),
-            new User(1L, "张三3",22),
-            new User(1L, "张三4",22),
-            new User(1L, "张三5",22)
+            new User(1L, "张三2",16),
+            new User(1L, "李四",22),
+            new User(1L, "张三4",19),
+            new User(1L, "王五",22)
     );
 
-    public List<String> getAdultUserName() {
+    public List<String> getAdultsUserName(String initial) {
         return users.stream()
                 .filter(user -> user.getAge() > 18)
                 .map(User::getName)
+                .map(String::trim) // 去除空格
+                .filter(name -> initial == null || name.startsWith(initial))
                 .collect(Collectors.toList());
     }
 }
