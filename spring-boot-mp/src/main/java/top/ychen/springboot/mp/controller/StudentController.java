f94f5f707e44a074a0de158c7ff04cff74eb60a5
Yeluzii <2317413623@qq.com>
2024-10-14 16:27:46 +0800

    spring-boot-mp: Mybatis Plus 模块

--- spring-boot-mp/src/main/java/top/ychen/springboot/mp/controller/StudentController.java
+++ spring-boot-mp/src/main/java/top/ychen/springboot/mp/controller/StudentController.java
@@ -14,6 +14,7 @@ import top.ychen.springboot.mp.service.StudentService;
 public class StudentController {
     private final StudentService studentService;
 
+
     @GetMapping("/{id}/courses")
     public Student getStudentWithCourses(@PathVariable Long id) {
         return studentService.getStudentWithCourses(id);
